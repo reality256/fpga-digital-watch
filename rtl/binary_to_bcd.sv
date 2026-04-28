a67dc1255e352228a84731ce2913d916cfa9ca44
reality256 <jiaming.li.256@outlook.com>
2026-04-28 03:51:13 +0000

    bcd_backup

---
+++ rtl/binary_to_bcd.sv
@@ -0,0 +1,13 @@
+//Binary to Binary-Coded Decimal
+module binary_to_bcd (
+    input  logic [6:0] bin,   // binary input (0 to 99)
+    output logic [3:0] tens,  // decimal tens digit
+    output logic [3:0] ones   // decimal ones digit
+);
+
+  logic [3:0] bcd[1:0];  // 2 BCD digits (tens and ones)
+
+
+
+
+endmodule
