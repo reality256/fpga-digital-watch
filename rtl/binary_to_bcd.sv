c7de6e9197505e65b23b4500a6d6d23b7f2e9621
reality256 <jiaming.li.256@outlook.com>
2026-05-05 01:37:08 +0000

    Binary to Binary-Coded Decimal

--- rtl/binary_to_bcd.sv
+++ rtl/binary_to_bcd.sv
@@ -1,13 +1,15 @@
 //Binary to Binary-Coded Decimal
+`timescale 1ns / 1ps
 module binary_to_bcd (
     input  logic [6:0] bin,   // binary input (0 to 99)
     output logic [3:0] tens,  // decimal tens digit
     output logic [3:0] ones   // decimal ones digit
 );
 
-  logic [3:0] bcd[1:0];  // 2 BCD digits (tens and ones)
-
-
-
+  always_comb begin
+    tens = 4'(bin / 7'd10);  // tens digit
+    ones = 4'(bin % 7'd10);  // ones digit
+  end
 
 endmodule
+
